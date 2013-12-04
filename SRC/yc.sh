90653e0f6504e2b21123e1011c3d599642b7ebe1
seduro <stefduro@gmail.com>
2013-12-04 20:28:17 +0100

    update yc.sh

--- SRC/yc.sh
+++ SRC/yc.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
-echo "===! Mosmlyacc !==="
-mosmlyac -v Parser.grm
-echo "===! Compile.sh !==="
-./compile.sh 
-echo "===! Paladim test files !==="
-  for file in ../DATA/*.pal
+{
+echo "===! Mosmlyacc !========================================================"
+mosmlyac -v Parser.grm;
+echo "===! Compile.sh !======================================================="
+./compile.sh;
+echo "===! Paladim test files !==============================================="
+  for file in ../DATA/*.pal #læser alle .pal filer i DATA
     do
-    echo "== $file =="
-  ../BIN/./Paladim $file >> paladim_results.out
-  done
-echo "===! End !==="
+    echo "== $file ==";
+  ../BIN/./Paladim $file > paladim-files.out
+  done 
+echo "===! End !=============================================================="
+} 2>&1 | tee output-yc.log #Output til log fil 2>&1 for at læse fra terminal
